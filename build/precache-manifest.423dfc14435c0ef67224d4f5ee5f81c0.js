c1d3686907e27a9e474888dff0e4ac66606ecef5
fhernan22 <fhern103@fiu.edu>
2019-09-10 18:14:33 -0400

    Added project files

---
+++ build/precache-manifest.423dfc14435c0ef67224d4f5ee5f81c0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ea0ad264b330981851d07961afafeb0d",
+    "url": "/React-Monsters-Search/index.html"
+  },
+  {
+    "revision": "1b1b7693accb997fff29",
+    "url": "/React-Monsters-Search/static/css/main.634f0418.chunk.css"
+  },
+  {
+    "revision": "d81979aa8ac68af22713",
+    "url": "/React-Monsters-Search/static/js/2.4ea1ec15.chunk.js"
+  },
+  {
+    "revision": "1b1b7693accb997fff29",
+    "url": "/React-Monsters-Search/static/js/main.e6898de5.chunk.js"
+  },
+  {
+    "revision": "5e326794d624c5bbfa15",
+    "url": "/React-Monsters-Search/static/js/runtime~main.565e80a3.js"
+  }
+]);
